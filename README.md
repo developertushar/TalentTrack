bbe2f4b8bf8357119ff700fb0ea056c6982ca93f
Mohit Bhagat <mbhagat751@outlook.com>
2018-02-08 12:17:25 +0530

    Designed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Vidya
+# VidyaPeeth
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.7.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { RouteModule } from'./routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { EventsComponent } from './events/events.component';
+import { AcademicsComponent } from './academics/academics.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    GalleryComponent,
+    ContactComponent,
+    EventsComponent,
+    AcademicsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
